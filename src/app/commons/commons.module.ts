ebd58f07ea01817959b0fc75e715afbfeac31438
Miguel Neto <miguelnetoarte@gmail.com>
2017-09-09 19:25:50 -0300

    componente header e manu exemplo

--- src/app/commons/commons.module.ts
+++ src/app/commons/commons.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  declarations: []
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ]
 })
 export class CommonsModule { }
