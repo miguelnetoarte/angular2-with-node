92a4df5f1a08918b71a8e1bf42358960ee893c55
Miguel Neto <miguelnetoarte@gmail.com>
2017-09-09 18:00:11 -0300

    rotas básicas e modulo de componentes comuns do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CommonsModule } from './commons/commons.module';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing,
+    CommonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
